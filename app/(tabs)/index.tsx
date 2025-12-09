a57248a9d77f3ebc06a6dde559a68248b83386dc
Kijeosowo <pelumiwilliams23@gmail.com>
2025-12-09 18:27:41 +0100

    added the top controls and user picture

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,102 +1,3 @@
-// import { Image } from 'expo-image';
-// import { Platform, StyleSheet } from 'react-native';
-
-// import { HelloWave } from '@/components/hello-wave';
-// import ParallaxScrollView from '@/components/parallax-scroll-view';
-// import { ThemedText } from '@/components/themed-text';
-// import { ThemedView } from '@/components/themed-view';
-// import { Link } from 'expo-router';
-
-// export default function HomeScreen() {
-//   return (
-//     <ParallaxScrollView
-//       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-//       headerImage={
-//         <Image
-//           source={require('@/assets/images/partial-react-logo.png')}
-//           style={styles.reactLogo}
-//         />
-//       }>
-//       <ThemedView style={styles.titleContainer}>
-//         <ThemedText type="title">Welcome!</ThemedText>
-//         <HelloWave />
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-//         <ThemedText>
-//           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-//           Press{' '}
-//           <ThemedText type="defaultSemiBold">
-//             {Platform.select({
-//               ios: 'cmd + d',
-//               android: 'cmd + m',
-//               web: 'F12',
-//             })}
-//           </ThemedText>{' '}
-//           to open developer tools.
-//         </ThemedText>
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <Link href="/modal">
-//           <Link.Trigger>
-//             <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-//           </Link.Trigger>
-//           <Link.Preview />
-//           <Link.Menu>
-//             <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-//             <Link.MenuAction
-//               title="Share"
-//               icon="square.and.arrow.up"
-//               onPress={() => alert('Share pressed')}
-//             />
-//             <Link.Menu title="More" icon="ellipsis">
-//               <Link.MenuAction
-//                 title="Delete"
-//                 icon="trash"
-//                 destructive
-//                 onPress={() => alert('Delete pressed')}
-//               />
-//             </Link.Menu>
-//           </Link.Menu>
-//         </Link>
-
-//         <ThemedText>
-//           {`Tap the Explore tab to learn more about what's included in this starter app.`}
-//         </ThemedText>
-//       </ThemedView>
-//       <ThemedView style={styles.stepContainer}>
-//         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-//         <ThemedText>
-//           {`When you're ready, run `}
-//           <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-//           <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-//           <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-//           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-//         </ThemedText>
-//       </ThemedView>
-//     </ParallaxScrollView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   titleContainer: {
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     gap: 8,
-//   },
-//   stepContainer: {
-//     gap: 8,
-//     marginBottom: 8,
-//   },
-//   reactLogo: {
-//     height: 178,
-//     width: 290,
-//     bottom: 0,
-//     left: 0,
-//     position: 'absolute',
-//   },
-// });
-
 import { useFonts } from "expo-font";
 import { useRouter } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -137,6 +38,18 @@ const artists = [
     image:
       "https://www.xclusiveland.com/wp-content/uploads/2025/05/BNXN-Buju-Fi-kan-We-kan-Ft-Rema.webp",
   },
+  {
+    id: 5,
+    songName: "Entergalactic",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzz9F3N-2UGLSuhzezsmNv3zcc2t5HN8libw&s",
+  },
+  {
+    id: 6,
+    songName: "Fi Kan We Kan",
+    image:
+      "https://www.xclusiveland.com/wp-content/uploads/2025/05/BNXN-Buju-Fi-kan-We-kan-Ft-Rema.webp",
+  },
 ];
 
 const Home = () => {
@@ -159,7 +72,73 @@ const Home = () => {
       <StatusBar barStyle="light-content" />
       <View
         style={{
-          marginTop: 50,
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          marginTop: 10,
+          marginBottom: 20,
+        }}
+      >
+        <Image
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/512/174/174872.png",
+          }}
+          style={{
+            width: 30,
+            height: 30,
+            borderRadius: 10,
+            marginRight: 0,
+          }}
+        />
+        <Text
+          style={{
+            color: "white",
+            backgroundColor: "#555",
+            paddingVertical: 5,
+            paddingHorizontal: 20,
+            borderRadius: 50,
+          }}
+        >
+          All
+        </Text>
+        <Text
+          style={{
+            color: "white",
+            backgroundColor: "#555",
+            paddingVertical: 5,
+            paddingHorizontal: 20,
+            borderRadius: 50,
+            borderWidth: 1,
+            borderColor: "white",
+          }}
+        >
+          Wrapped
+        </Text>
+        <Text
+          style={{
+            color: "white",
+            backgroundColor: "#555",
+            paddingVertical: 5,
+            paddingHorizontal: 20,
+            borderRadius: 50,
+          }}
+        >
+          Music
+        </Text>
+        <Text
+          style={{
+            color: "white",
+            backgroundColor: "#555",
+            paddingVertical: 5,
+            paddingHorizontal: 20,
+            borderRadius: 50,
+          }}
+        >
+          Podcast
+        </Text>
+      </View>
+      <View
+        style={{
+          marginTop: 0,
           marginBottom: 10,
           paddingHorizontal: 20,
           flexDirection: "row",
@@ -172,18 +151,18 @@ const Home = () => {
             key={artist.songName}
             style={{
               width: "48%",
-              marginBottom: 20,
+              marginBottom: 10,
               backgroundColor: "#333",
-              padding: 10,
-              borderRadius: 10,
+              padding: 0,
+              borderRadius: 5,
             }}
           >
             <View style={{ flexDirection: "row", alignItems: "center" }}>
               <Image
                 source={{ uri: artist.image }}
                 style={{
-                  width: 50,
-                  height: 50,
+                  width: 60,
+                  height: 60,
                   borderRadius: 10,
                   marginRight: 10,
                 }}
